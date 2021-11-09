012f33a01815a86269aba65c1795361bd66f89bf
Ana Catarina Marques Vicentim <catarinaanamarques830@gmail.com>
2021-11-09 10:03:38 -0300

    Add files via upload

---
+++ salario_funcionario_ajustado_3.cpp
@@ -0,0 +1,34 @@
+//Diretivas de Pr�-processamento (Obrigat�rias)
+#include <stdio.h>
+#include <stdlib.h>
+//Obrigat�rio. Fun��o principal: indica o in�cio da execu��o do programa
+int main ()
+{	
+	//Declara��o de Vari�veis Locais. 
+	float sal_func;
+ 	//Obrigat�rio. Comandos para resolu��o do problema
+ 	printf("Digite o sal�rio do funcion�rio: ");
+ 	scanf("%f",&sal_func);
+ 	
+	if (sal_func<500)
+	{
+		printf("\nO sal�rio do funcion�rio �: %.2f", sal_func);
+ 		printf("\nO sal�rio com reajuste �: %.2f", sal_func+sal_func*0.15);
+ 	
+	}
+	else if (sal_func>=500 && sal_func<=1000)
+	{
+		printf("\nO sal�rio do funcion�rio �: %.2f", sal_func);
+ 		printf("\nO sal�rio com reajuste �: %.2f", sal_func+sal_func*0.10);
+ 	}	
+	else 
+	{
+		printf("\nO sal�rio do funcion�rio �: %.2f", sal_func);
+ 		printf("\nO sal�rio com reajuste �: %.2f", sal_func+sal_func*0.05);
+ 	}	
+	//Opcional. Comando para interromper momentaneamente o programa
+  	printf("\n");
+  	system("PAUSE");	
+  	//Retorno ao SO o status do programa
+  	return 0;
+}//Indica o final do programa.
