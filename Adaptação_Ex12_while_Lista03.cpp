71ba4d7192cd1b044f1ce441567bef1f7adf0c2d
Ana Catarina Marques Vicentim <catarinaanamarques830@gmail.com>
2021-11-09 09:43:18 -0300

    Add files via upload

---
+++ Adaptação_Ex12_while_Lista03.cpp
@@ -0,0 +1,42 @@
+//Diretivas de Pr�-processamento (Obrigat�rias)
+#include <stdio.h>
+
+//Obrigat�rio. Fun��o principal: indica o in�cio da execu��o do programa
+
+int main ()
+{	
+	//Declara��o de Vari�veis Locais. 
+	int num, maior, menor, i;
+	//Obrigat�rio. Comandos para resolu��o do problema
+	//Poss�veis mensagens para indicar as entradas
+ 	printf("\nDigite cinco n�meros:");
+	i=0;
+	while (i<5)
+	{
+		printf("\nDigite um n�mero (%d): ", i);
+		scanf("%d",&num);
+		
+		if (i==0) //inicializa��o para compara��o
+		{ 
+			maior=num;
+			menor=num;
+		}
+		else if (num>maior)//Identifica��o do maior n�mero e a sa�da
+		{
+			maior=num;
+		}
+		else if (num<menor)//Identifica��o do menor n�mero e a sa�da
+		{
+			menor=num;
+		}
+		i++;
+	}
+	printf("\n O maior numero �: %d", maior);
+	printf("\n O menor numero �: %d", menor);
+	
+  	printf("\n");
+  	system("PAUSE");
+  	//Retorno ao SO o status do programa
+  	return 0;
+}//Indica o final do programa.
+
