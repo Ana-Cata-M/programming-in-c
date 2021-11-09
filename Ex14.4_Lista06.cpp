9366de30a55000672fed7153b9c88aa4fad4ef5a
Ana Catarina Marques Vicentim <catarinaanamarques830@gmail.com>
2021-11-09 09:53:57 -0300

    Add files via upload

---
+++ Ex14.4_Lista06.cpp
@@ -0,0 +1,88 @@
+//Exerc�cio 14.4
+
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+  setlocale(LC_ALL,"Portuguese");
+  int tempo[7][7]={
+  {0,2,11,6,15,11,1},
+  {2,0,7,12,4,2,15},
+  {11,7,0,11,8,3,13},
+  {6,12,11,0,10,2,1},
+  {15,4,8,10,0,5,13},
+  {11,2,3,2,5,0,14},
+  {1,15,13,1,13,14,0}
+  };
+  int origem, destino, primeira, segunda;
+   
+  for(int i=0; i<7; i++)
+  {
+    printf("\n");
+    for(int j=0; j<7; j++)
+    {  
+      printf(" [%d] ",tempo[i][j]);
+    }
+  }
+    printf("\n\n");
+  do{
+        printf("\nDigite a cidade de origem: ");
+        scanf("%d",&origem);
+        if (origem<0 || origem>6)
+        {
+          printf("\nPosi��o fora do intervalo da matriz");
+        }
+    }while(origem<0 || origem>6);
+  
+  do{
+        printf("\nDigite a primeira op��o de cidade para pernoite: ");
+        scanf("%d",&primeira);
+        if (primeira<0 || primeira>6)
+        {
+          printf("\nPosi��o fora do intervalo da matriz");
+        }
+    }while(primeira<0 || primeira>6);
+  do{
+        printf("\nDigite a segunda op��o de cidade para pernoite: ");
+        scanf("%d",&segunda);
+        if (segunda<0 || segunda>6)
+        {
+          printf("\nPosi��o fora do intervalo da matriz");
+        }
+    }while(segunda<0 || segunda>6);
+  
+  do{
+      printf("\nDigite a cidade de destino: ");
+      scanf("%d",&destino);
+      if (destino<0 || destino>6)
+      {
+        printf("\nPosi��o fora do intervalo da matriz");
+      }
+    }while(destino<0 || destino>6);
+    
+  //Verifica qual percurso demanda menos tempo
+  if((tempo[origem][primeira]+tempo[primeira][destino])<(tempo[origem][segunda]+tempo[segunda][destino]))
+  {
+    printf("\nO percurso com o menor tempo total � o que se usa a primeira op��o de pernoite:");
+    printf("\nOrigem -- %d -- Primeira op. pernoite -- %d: Tempo %d", origem, primeira, tempo[origem][primeira]);
+    printf("\nPrimeira op. pernoite -- %d -- Destino -- %d: Tempo %d ",primeira,destino,tempo[primeira][destino]);;
+    printf("\nTempo total: %d",tempo[origem][primeira]+tempo[primeira][destino]);
+  } 
+  //verifica se os percursos s�o iguais
+  else if((tempo[origem][primeira]+tempo[primeira][destino])==(tempo[origem][segunda]+tempo[segunda][destino]))
+  {
+    printf("Ambos os percursos tem o mesmo tempo final de %d ",(tempo[origem][primeira]+tempo[primeira][destino]));
+  }
+  else //caso contr�rio, a segunda op��o de pernoite produz um percurso que demanda menos tempo
+  {
+    printf("\nO percurso com o menor tempo total � o que se usa a segunda op��o de pernoite:");
+    printf("\nOrigem -- %d -- Segunda op. pernoite -- %d: Tempo %d\n", origem, segunda, tempo[origem][segunda]);
+    printf("\nSegunda op. pernoite -- %d -- Destino -- %d: Tempo %d\n",segunda,destino,tempo[segunda][destino]);;
+    printf("\nTempo total: %d",tempo[origem][segunda]+tempo[segunda][destino]);
+  
+  }
+  //system (Pause)
+  return 0; 
+}
+
